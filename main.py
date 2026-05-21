45f287490b531c912fae527f204232164ca0befc
Belyakov04 <kirill.belyakov.04@mail.ru>
2026-05-21 12:45:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widget_serialization
+from matplotlib.patches import Rectangle
+import time
+from IPython.display import clear_output
+
+
